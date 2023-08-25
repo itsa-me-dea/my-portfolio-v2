9c3710e47bc426aff3df4595501d7ec1d6f9912c
itsa-me-dea <wachadea@gmail.com>
2023-08-24 19:05:55 -0700

    updated resume

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -2,18 +2,27 @@ export default function Resume() {
     return (
       <div>
         <h1>My Resume</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-          velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-          ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-          non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-          ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-          rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-          tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-          porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-          vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-          fames ac ante ipsum primis in faucibus.
-        </p>
+        <h5>TECHNICAL SKILLS</h5>
+        <p><b>Developer:</b> JavaScript ES6+, CSS3, HTML5, NoSQL, GitHub, MongoDB, MySQL, Express, React, Node, Handlebars, jQuery, Bootstrap</p>
+        <p><b>Lab:</b> UV-VIS Spectrometer, Microscopy, SEM, HPLC, GC, Viscometer, pH Meter, Osmometer, iCE3 cIEF, DLS, Solo VPE, GMP, Data Review</p>
+        <h5>EXPERIENCE </h5>
+        <p><b>Scientist I | Eurofins Advantar Lab |  04/2023 - Present</b></p>
+        <p>Including the associate scientist duties, working in both GMP and non-GMP environments, acting as both analyst and data reviewer for notebooks, reports, and data.</p>
+        <ul>
+          <li>Performs various tasks and analysis of Bio/Pharmaceutical materials, including running a variety of equipment and instrumentation independently.</li>
+          <li>Ensuring quality data with accuracy, clarity and adherence to cGMP regulations and/or support cGLP studies.</li>
+        </ul>
+        <p><b>Associate Scientist | Eurofins Advantar Lab |  09/2021 - 04/2023</b></p>
+        <p>Executed plans developed by supervisor and followed established procedures to complete routine testing; may develop solutions for basic technical problems.</p>
+        <ul>
+          <li>Focused on meeting needs of Sponsors, in the context of outputs to the next stage of workflow, and customer expectations.</li>
+          <li>Communicated effectively by writing clear, concise technical conclusions in laboratory notebooks.</li>
+          <li>Support LEAN initiatives and comply with Health, Safety & Environmental responsibilities. Conducts all activities in a safe and efficient manner.</li>
+        </ul>
+        <h5>EDUCATION</h5>
+        <p><b>Certificate, Full Stack Web Development</b> – University of California Extension, San Diego | 09/2023</p>
+        <p><b>M.S. Biomedical Nanoengineering</b> – University of California, San Diego | 06/2021</p>
+        <p><b>B.S. Nanoengineering</b> – University of California, San Diego | 06/2020</p>
       </div>
     );
   }
