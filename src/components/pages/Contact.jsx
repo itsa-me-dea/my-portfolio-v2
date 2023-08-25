18642ac4f788afa68cd0ce3b7dd14fa1c72e5245
itsa-me-dea <wachadea@gmail.com>
2023-08-24 20:44:54 -0700

    started forms on contact

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,20 +1,73 @@
-export default function Contact() {
+import { useState } from 'react';
+import './style.css';
+
+function Contact() {
+  // Here we set two state variables for firstName and name using `useState`
+  const [firstName, setFirstName] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleNameChange = (e) => {
+    const { name, value } = e.target;
+    return name === setName(value);
+  };
+  
+  const handleEmailChange = (e) => {
+    const { email, value } = e.target;
+    return email === setEmail(value);
+  };
+
+  const handleMessageChange = (e) => {
+    const { message, value } = e.target;
+    return message === setMessage(value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    alert(`Hello ${name}`);
+    setName('');
+  };
+
   return (
-    <div>
-      <h1>Contact Me</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+    <div className="container text-center">
+      <h1>
+        Contact Me
+      </h1>
+      <form className="form" onSubmit={handleFormSubmit}>
+        <input
+          value={name}
+          name="name"
+          onChange={handleNameChange}
+          type="text"
+          placeholder="Name"
+          required
+        />
+        <input
+          value={email}
+          name="email"
+          onChange={handleEmailChange}
+          type="text"
+          placeholder="Email"
+          required
+        />
+        <div className="input-container">
+          <textarea 
+            className="custom-input"
+            value={message}
+            name="message"
+            onChange={handleMessageChange}
+            type="text" 
+            placeholder="Enter your message"
+          >
+          </textarea>
+        </div>
+        <button type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
+
+export default Contact;
