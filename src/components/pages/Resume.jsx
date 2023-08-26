8460dcd08e076bff128e8a3aa59ed2dbc616c51f
itsa-me-dea <wachadea@gmail.com>
2023-08-26 03:05:02 -0700

    updated resume page

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -4,29 +4,26 @@ const centerText = {
 
 export default function Resume() {
     return (
-      <div>
+      <div className='resume'>
         <h1 style={centerText}>My Resume</h1>
         <h5>TECHNICAL SKILLS</h5>
         <p><b>Developer:</b> JavaScript ES6+, CSS3, HTML5, NoSQL, GitHub, MongoDB, MySQL, Express, React, Node, Handlebars, jQuery, Bootstrap</p>
         <p><b>Lab:</b> UV-VIS Spectrometer, Microscopy, SEM, HPLC, GC, Viscometer, pH Meter, Osmometer, iCE3 cIEF, DLS, Solo VPE, GMP, Data Review</p>
         <h5>EXPERIENCE </h5>
-        <p><b>Scientist I | Eurofins Advantar Lab |  04/2023 - Present</b></p>
-        <p>Including the associate scientist duties, working in both GMP and non-GMP environments, acting as both analyst and data reviewer for notebooks, reports, and data.</p>
-        <ul>
-          <li>Performs various tasks and analysis of Bio/Pharmaceutical materials, including running a variety of equipment and instrumentation independently.</li>
-          <li>Ensuring quality data with accuracy, clarity and adherence to cGMP regulations and/or support cGLP studies.</li>
-        </ul>
-        <p><b>Associate Scientist | Eurofins Advantar Lab |  09/2021 - 04/2023</b></p>
-        <p>Executed plans developed by supervisor and followed established procedures to complete routine testing; may develop solutions for basic technical problems.</p>
-        <ul>
-          <li>Focused on meeting needs of Sponsors, in the context of outputs to the next stage of workflow, and customer expectations.</li>
-          <li>Communicated effectively by writing clear, concise technical conclusions in laboratory notebooks.</li>
-          <li>Support LEAN initiatives and comply with Health, Safety & Environmental responsibilities. Conducts all activities in a safe and efficient manner.</li>
-        </ul>
+        <p><b>Scientist I</b> | Eurofins Advantar Lab |  04/2023 - Present</p>
+      
+        <p><b>Associate Scientist</b> | Eurofins Advantar Lab |  09/2021 - 04/2023</p>
+        
         <h5>EDUCATION</h5>
         <p><b>Certificate, Full Stack Web Development</b> – University of California Extension, San Diego | 09/2023</p>
         <p><b>M.S. Biomedical Nanoengineering</b> – University of California, San Diego | 06/2021</p>
         <p><b>B.S. Nanoengineering</b> – University of California, San Diego | 06/2020</p>
+        {/* Resume pdf */}
+        <div className='pdf'>
+          <form action="https://drive.google.com/file/d/1-U3Z8IV5M7s9kRAIynrsIcPWmuTWFlMN/view?usp=sharing">
+            <button type="submit">Open Resume</button>
+          </form>
+        </div>
       </div>
     );
   }
