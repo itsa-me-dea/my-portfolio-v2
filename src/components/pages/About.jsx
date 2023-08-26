724a1a190122651132359366258934bde5c716da
itsa-me-dea <wachadea@gmail.com>
2023-08-26 15:14:53 -0700

    updated SEO for About img

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -24,7 +24,7 @@ export default function About() {
     <div>
       <h1 style={centerText}>About Me</h1>
       <div className='about-container' style={flexContainer}>
-        <img id="my-pic" src={kirbo} alt="kirbo" style={imgStyle} />
+        <img id="my-pic" src={kirbo} alt="TerminalMontage Kirbo | itsa-me-dea GitHub Avatar" style={imgStyle} />
         <div>
           <p>Hi there. Hello!</p>
           <p>My name is Nhadea Lensky. I am a nerd for anything STEM or gaming related. I am new to the code game, but so far my experience has only made me more excited to learn. Thus far, I have dabbled in HTML, CSS, JavaScript and MERN stack development. I believe that I have a stronger understanding for front-end applications, but I am also comfortable with database management.</p>
