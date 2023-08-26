43ea3114ab5835dff07905a8066c42cdeb2378a6
itsa-me-dea <wachadea@gmail.com>
2023-08-26 00:02:09 -0700

    updated portfolio page to contain more info

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,6 +1,16 @@
 import { useState } from 'react';
 import './Portfolio.css';
-import kirbo from '../../images/kirbo-test.gif';
+import './style.css';
+// IMAGES
+import JATE from '../../images/kirbo-test.gif';
+import socialDB from '../../images/kirbo-test.gif';
+import expressNotes from '../../images/kirbo-test.gif';
+import README from '../../images/kirbo-test.gif';
+import scheduler from '../../images/kirbo-test.gif';
+import quiz from '../../images/kirbo-test.gif';
+import password from '../../images/kirbo-test.gif';
+import studyGuide from '../../images/kirbo-test.gif';
+
 
 export default function Portfolio() {
   const [focusedImage, setFocusedImage] = useState(null);
@@ -15,34 +25,60 @@ export default function Portfolio() {
 
   const portfolioImages = [
     {
-      src: kirbo,
+      src: JATE,
+      alt: 'just another text editor',
+      overlayText: 'J.A.T.E.',
+      repo: 'https://github.com/itsa-me-dea/just-another-text-editor',
+      link: 'https://lit-ocean-67030-1a0913892dd0.herokuapp.com/',
+    },
+    {
+      src: socialDB,
       alt: 'Image 1',
-      overlayText: 'Project 1',
+      overlayText: 'Social Media Database',
+      repo: 'https://github.com/itsa-me-dea/NoSQL-SocialMedia-DB',
+      link: '',
     },
     {
-      src: kirbo,
-      alt: 'Image 2',
-      overlayText: 'Project 2',
-    },
-    {
-      src: kirbo,
+      src: expressNotes,
       alt: 'Image 1',
-      overlayText: 'Project 3',
+      overlayText: 'Note Taker',
+      repo: 'https://github.com/itsa-me-dea/Express-Note-Taker',
+      link: 'https://agile-spire-31422-c2116f810f76.herokuapp.com/notes',
     },
     {
-      src: kirbo,
+      src: README,
       alt: 'Image 2',
-      overlayText: 'Project 4',
+      overlayText: 'README Generator',
+      repo: 'https://github.com/itsa-me-dea/Node-README-Generator',
+      link: '',
     },
     {
-      src: kirbo,
+      src: scheduler,
       alt: 'Image 1',
-      overlayText: 'Project 5',
+      overlayText: 'Workday Scheduler',
+      repo: 'https://github.com/itsa-me-dea/05-Challenge-WorkDayScheduler',
+      link: 'https://itsa-me-dea.github.io/05-Challenge-WorkDayScheduler/',
     },
     {
-      src: kirbo,
+      src: quiz,
       alt: 'Image 2',
-      overlayText: 'Project 6',
+      overlayText: 'Coding Quiz',
+      repo: 'https://github.com/itsa-me-dea/04-Challenge-CodeQuiz',
+      link: 'https://itsa-me-dea.github.io/04-Challenge-CodeQuiz/',
+    },
+    {
+      src: password,
+      alt: 'Image 1',
+      overlayText: 'Password Generator',
+      repo: 'https://github.com/itsa-me-dea/03-Challenge-PasswordGenerator',
+      link: 'https://itsa-me-dea.github.io/03-Challenge-PasswordGenerator/',
+    },
+    {
+      src: studyGuide,
+      alt: 'Image 2',
+      overlayText: 'Prework Study Guide',
+      repo: 'https://github.com/itsa-me-dea/prework-study-guide',
+      link: 'https://itsa-me-dea.github.io/prework-study-guide/',
     },
   ];
 
@@ -59,7 +95,21 @@ export default function Portfolio() {
             tabIndex={0}
           >
             <img src={image.src} alt={image.alt} />
-            <div className="overlay">{image.overlayText}</div>
+            <div className="overlay">
+              <div className="overlay-links">
+                <div className="link-item">
+                  {image.overlayText}
+                </div>
+                <div className="link-item">
+                  <a href={image.repo}>Repo</a>
+                </div>
+                {image.link && (
+                <div className="link-item">
+                  <a href={image.link}>Link</a>
+                </div>
+                )}
+              </div>
+            </div>
           </div>
         ))}
       </div>
