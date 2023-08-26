2f7321401dedd59698ebcff050247f535b9c57de
itsa-me-dea <wachadea@gmail.com>
2023-08-25 22:54:26 -0700

    updated footer with links

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,3 +1,7 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
+
 const footerStyle = {
   backgroundColor: 'black',
   color: 'white',
@@ -5,10 +9,27 @@ const footerStyle = {
   textAlign: 'center',
 };
 
+const iconStyle = {
+  margin: '10px',
+};
+
 function MyFooter() {
   return (
     <footer style={footerStyle}>
-      <p>*Insert Links Here*</p>
+      {/* LinkedIn */}
+      <a href="https://www.linkedin.com/in/nhadea-lensky-567158210/" target="_blank" rel="noopener noreferrer" style={iconStyle}>
+        <FontAwesomeIcon icon={faLinkedin} size="2xl" />
+      </a>
+
+      {/* GitHub */}
+      <a href="https://github.com/itsa-me-dea" target="_blank" rel="noopener noreferrer" style={iconStyle}>
+      <FontAwesomeIcon icon={faGithub} size="2xl" />
+      </a>
+
+      {/* Stack Overflow */}
+      <a href="https://stackoverflow.com/users/21659831/its-a-me-dea" target="_blank" rel="noopener noreferrer" style={iconStyle}>
+      <FontAwesomeIcon icon={faStackOverflow} size="2xl" />      
+      </a>
     </footer>
   );
 }
